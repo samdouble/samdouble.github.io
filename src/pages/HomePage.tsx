0c38cb8b4c52df937f0f8939ccc575d3fd70b581
samdouble <samleafs@protonmail.com>
2024-09-18 21:39:29 -0400

    feat: get npm and PyPI logos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import PostsList from 'components/PostsList';
+import SocialMedia from 'components/SocialMedia';
 import content from 'content.json';
 
 function HomePage() {
@@ -16,33 +17,6 @@ function HomePage() {
     >
       <Row>
         <Col lg={8}>
-          <a href="https://forums.docker.com/u/samdouble/summary">
-            <img src="/assets/icons/docker.webp" alt="Docker" width="60"/>
-          </a>
-
-          <a href="https://github.com/samdouble">
-            <img src="/assets/icons/github.png" alt="GitHub" width="60"/>
-          </a>
-
-          <a href="https://medium.com/@samdouble">
-            <img src="/assets/icons/medium.png" alt="Medium" width="60"/>
-          </a>
-
-          <a href="https://community.snowflake.com/s/profile/005VI000000oojB">
-            <img src="/assets/icons/snowflake.png" alt="Snowflake" width="60"/>
-          </a>
-
-          <a href="https://stackoverflow.com/users/12787394/samdouble">
-            <img src="/assets/icons/stackoverflow.webp" alt="GitHub" width="60"/>
-          </a>
-
-          <a href="https://www.npmjs.com/~samdouble">
-            <img src="/assets/icons/stackoverflow.webp" alt="npm" width="60"/>
-          </a>
-
-          <a href="https://pypi.org/user/samdouble/">
-            <img src="/assets/icons/stackoverflow.webp" alt="PyPI" width="60"/>
-          </a>
         </Col>
         <Col lg={4}>
           <div>
@@ -50,6 +24,8 @@ function HomePage() {
             <PostsList
               posts={latestPosts}
             />
+            <h2>Social Media</h2>
+            <SocialMedia />
           </div>
         </Col>
       </Row>
