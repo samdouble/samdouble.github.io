a306749030c4ff8eb0ebf62e06a24d2d1f33169b
samdouble <samleafs@protonmail.com>
2024-09-24 08:12:02 -0400

    ci: set up ESlint

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,37 +5,65 @@ function SocialMedia() {
     <div>
       <div>
         <a href="https://github.com/samdouble">
-          <img src="/assets/icons/github.png" alt="GitHub" width="60"/>
+          <img
+            src="/assets/icons/github.png"
+            alt="GitHub"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://medium.com/@samdouble">
-          <img src="/assets/icons/medium.png" alt="Medium" width="60"/>
+          <img
+            src="/assets/icons/medium.png"
+            alt="Medium"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://snowflake.discourse.group/u/samdouble/summary">
-          <img src="/assets/icons/snowflake.png" alt="Snowflake" width="60"/>
+          <img
+            src="/assets/icons/snowflake.png"
+            alt="Snowflake"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://stackoverflow.com/users/12787394/samdouble">
-          <img src="/assets/icons/stackoverflow.webp" alt="GitHub" width="60"/>
+          <img
+            src="/assets/icons/stackoverflow.webp"
+            alt="GitHub"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://www.npmjs.com/~samdouble">
-          <img src="/assets/icons/npm.webp" alt="npm" width="60"/>
+          <img
+            src="/assets/icons/npm.webp"
+            alt="npm"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://pypi.org/user/samdouble/">
-          <img src="/assets/icons/pypi.png" alt="PyPI" width="60"/>
+          <img
+            src="/assets/icons/pypi.png"
+            alt="PyPI"
+            width="60"
+          />
         </a>
       </div>
       <div>
         <a href="https://forums.docker.com/u/samdouble/summary">
-          <img src="/assets/icons/docker.webp" alt="Docker" width="60"/>
+          <img
+            src="/assets/icons/docker.webp"
+            alt="Docker"
+            width="60"
+          />
         </a>
       </div>
     </div>
